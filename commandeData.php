a1016f499be37d590e05960eee956c35a794d3bf
ChaelPix <55618696+ChaelPix@users.noreply.github.com>
2022-11-17 11:55:02 +0100

    bdd

--- commandeData.php
+++ commandeData.php
@@ -75,6 +75,10 @@ if ($valEnvoyer == "Envoyer") {
     print("4) Quantite de blanc pour : C $whiteForCyan ml | M $whiteForMagenta ml | Y $whiteForYellow ml | B $whiteForBlack ml <br>");
     $whiteQuantity = $whiteForCyan+$whiteForMagenta+$whiteForYellow+$whiteForBlack;
 
+    if ($black < 1){
+        $blackQuantity = $peintQuantMl;
+        $whiteQuantity = 0;
+    }
     //SaveFile
     $fileName = "$nomClient $prenomClient.txt";
     $monFich = fopen($fileName, "w");
@@ -87,30 +91,75 @@ if ($valEnvoyer == "Envoyer") {
     fputs($monFich, $toSave);
     fclose($monFich);
 
-    //DataBase
-    //Connect to the DB
-    $bdd = new mysqli('10.187.52.4', 'beaujardt', 'beaujardt');
+    //-----------------------------DataBase
+    //Connect to DB
+    $bdd = new mysqli('10.187.52.4', 'beaujardt', 'beaujardt', 'beaujardt_b');
     if ($bdd->connect_errno) {
         print("ERROR of connexion <br>"); 
         exit();
     }
-    print("Connected to DB <br>");
+    print("Connected to DB <br>"); 
 
-    //Create DB
-    $sql = "CREATE DATABASE myDB";
-    if (mysqli_query($bdd, $sql)) {
-      echo "Database created successfully";
-    } else {
-      echo "Error creating database: " . mysqli_error($bdd);
+    //Check the Stock
+    $requete = "select * from stock";
+    $resultat=$bdd->query($requete); // requête sur le stock
+    if($resultat==TRUE){
+    $ligne = $resultat->fetch_assoc(); // cette méthode permet d'obtenir un tableau sur le stock
+    $noir = $ligne['quantite_noir'];
+    $white = $ligne['quantite_blanc'];
+    $cyan = $ligne['quantite_cyan'];
+    $yellow = $ligne['quantite_yellow'];
+    $magenta = $ligne['quantite_magenta'];
+
+    echo "5). $noir. $white. $cyan. $yellow. $magenta";
     }
 
-    //Get DB Values
-    $requete = "select * fromclient";
-    if($bdd->query($requete) == TRUE) {
-        echo "Request OK"; 
-        echo $bdd->affected_rows;
-       }
-       else echo "Request Error";
+    //Calcul remaining stocks
+    $white -= $whiteQuantity;
+    $noir -= $blackQuantity;
+    $cyan -= $cyanQuantity;
+    $yellow -= $yellowQuantity;
+    $magenta -= $magentaQuantity;
+    if($white < 0 || $black < 0 || $cyan < 0 || $yellow < 0 || $magenta < 0)
+    {
+        print("<h2> Order cancelled, no enought stocks! </h2>");
+        return;
+    }
+
+    //Update Stocks
+    $requete = "UPDATE `stock` SET `id_stock`=1,`quantite_noir`=$noir,`quantite_blanc`=$white,`quantite_cyan`=$cyan,`quantite_magenta`=$magenta,`quantite_yellow`=$yellow WHERE 1";
+    $resultat = $bdd->query($requete);
+    if ($resultat == FALSE) die("Error Update Stocks. $bdd->error;");
+
+    //Add Client
+    $query = "SELECT * FROM client WHERE nom = '$nomClient' && prenom = '$prenomClient'";
+    $result = $bdd->query($query);
+    if ($result) {
+        if (mysqli_num_rows($result) > 0) {
+            echo '<br> client already registered!';
+        } else {
+            echo '<br> new client';
+            $requete = "INSERT INTO `client`(`id_client`, `nom`, `prenom`, `adresse`) VALUES (null,'$nomClient','$prenomClient','$adresseClient')";
+            $resultat = $bdd->query($requete);
+            if ($resultat == FALSE) die("Error Update Client. $bdd->error;");
+        }
+    } 
     
+    //Add Commande Data
+    $currentDate = date('Y-m-d H:i:s');
+
+    $query = "SELECT * FROM client WHERE nom = '$nomClient' && prenom = '$prenomClient'";
+    $resultat = $bdd->query($query);
+    if ($resultat == TRUE) {
+        $ligne = $resultat->fetch_assoc(); 
+        $id = $ligne['id_client']; 
+    } 
+
+    $requete = "INSERT INTO `commande`(`id_commande`, `id_client`, `id_stock`, `nb_blanc`, `nb_noir`, `nb_cyan`, `nb_magenta`, `nb_yellow`, `prep`)
+    VALUES (null,$id,1,$whiteQuantity,$blackQuantity,$cyanQuantity,$magentaQuantity,$yellowQuantity,'$currentDate')"; //Recup ID Client
+    $resultat = $bdd->query($requete);
+    if($resultat == FALSE) die("Error Update Order. $bdd->error;"); 
+
+    print("<h2> Order Sucess! </h2>");
     $bdd->close();
 }
